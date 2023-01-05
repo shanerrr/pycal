78abfe3a5c5d73dd67ce6f06261cdd4e988d3c5d
Shane Goonasekera <theshanukg@gmail.com>
2023-01-05 15:57:04 -0700

    added a way to support an arbitrary number of arguments

--- calc.py
+++ calc.py
@@ -1,27 +1,30 @@
 import sys
-
-open_list = ["[", "{", "("]
-close_list = ["]", "}", ")"]
+import math
 
 
 class Calculator:
 
     def compute(self, input):
+        # if there is an opening bracket, we know its an expression
+        while '(' in input:
+            # getting the starting and ending bracket of deepest nested expression
+            rb = input.find(')')
+            lb = input[:rb].rfind('(')
 
-        rb = input.find(')')
-        lb = input[:rb].rfind('(')
+            # call our evaluate method with the most nested expression
+            solution = self._evaluate(
+                input[lb + 1:rb])
 
-        solution = self._evaluate(
-            input[lb + 1:rb])
+            # when the left bracket idx is 0, we know we are at the root expression
+            if lb == 0:
+                return solution
 
-        return solution
-        # for index in range(len(input)):
-        #     if input[index] in open_list:
-        #         self.stack.append(index)
-        #     elif input[index] in close_list:
-        #         solution = self._evaluate(input[self.stack.pop()+1:index])
-        #         if not self.stack:
-        #             return solution
+            # otherwise, update the calculated expression in string with the calculated value
+            else:
+                input = input[:lb] + str(solution) + input[rb+1:]
+
+        # if not brackets, we can assume its just an interger
+        return int(input)
 
     def _evaluate(self, expression):
 
@@ -31,17 +34,19 @@ class Calculator:
 
         match args[0]:
             case 'add':
-                answer = self._add(args[1], args[2])
+                answer = self._add(args[1:])
             case 'multiply':
-                answer = self._multiply(args[1], args[2])
+                answer = self._multiply(args[1:])
 
         return answer
 
-    def _add(self, x, y):
-        return int(x)+int(y)
+    def _add(self, values):
+        # turn to a list of ints and add them all up
+        return sum(list(map(int, values)))
 
-    def _multiply(self, x, y):
-        return int(x)*int(y)
+    def _multiply(self, values):
+        # turn to a list of ints and multiplys them all up using math module
+        return math.prod(list(map(int, values)))
 
 
 def main():
